6d5f7c38f60662c5dd8a13118c3bb72d1090e287
Firmansyah Rozy <firmansyah.rozy@salt.co.id>
2022-09-11 08:14:06 +0700

    refactor main function

--- main.go
+++ main.go
@@ -2,67 +2,23 @@ package main
 
 import (
 	"log"
-	"sync"
-	"time"
+	"net/http"
+
+	"github.com/rozy97/pizzahub/src/handler"
 )
 
-type Chef struct {
-	ID   int
-	Name string
-}
-
-type Product struct {
-	ID   int
-	Name string
-}
-
-func AddChefToKithen(kitchen chan *Chef, chef *Chef, msg string) {
-	kitchen <- chef
-	log.Printf("chef %s %s to kitchen", chef.Name, msg)
-
-}
-
-func GetChefFromKitchen(kitchen chan *Chef) *Chef {
-	chef := <-kitchen
-	return chef
-}
-
-func CookPizza(kitchen chan *Chef, orderID int) {
-	chef := GetChefFromKitchen(kitchen)
-	log.Printf("start cookin pizza from order %v", orderID)
-	time.Sleep(3 * time.Second)
-	AddChefToKithen(kitchen, chef, "back from cooking pizza")
-	result := &Product{
-		ID:   1,
-		Name: "pizza",
-	}
-	log.Printf("%s cooked by %s!\n", result.Name, chef.Name)
-}
-
 func main() {
-	var wg sync.WaitGroup
-	kitchen := make(chan *Chef, 1000)
-	chef1 := &Chef{
-		ID:   1,
-		Name: "arnold",
-	}
-	chef2 := &Chef{
-		ID:   2,
-		Name: "juna",
-	}
 
-	wg.Add(2)
-	go AddChefToKithen(kitchen, chef1, "added")
-	go func() {
-		CookPizza(kitchen, 1)
-		defer wg.Done()
-	}()
-	go func() {
-		CookPizza(kitchen, 2)
-		defer wg.Done()
-	}()
-	time.Sleep(2 * time.Second)
-	go AddChefToKithen(kitchen, chef2, "added")
-	wg.Wait()
+	chefHandler := handler.NewChefHTTPHandler()
+	menuHandler := handler.NewMenuHTTPHandler()
+	orderHandler := handler.NewOrderHTTPHandler()
 
+	http.HandleFunc("/chef", chefHandler.Chef)
+	http.HandleFunc("/menus", menuHandler.Menus)
+	http.HandleFunc("/orders", orderHandler.Order)
+
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		log.Println(err.Error())
+	}
 }
