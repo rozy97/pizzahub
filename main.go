feb35ea5b357a81d978ab821a56640f2dd7676d6
Firmansyah Rozy <firmansyah.rozy@salt.co.id>
2022-09-11 10:32:55 +0700

    call dependencies in main function

--- main.go
+++ main.go
@@ -5,13 +5,23 @@ import (
 	"net/http"
 
 	"github.com/rozy97/pizzahub/src/handler"
+	"github.com/rozy97/pizzahub/src/repository"
+	"github.com/rozy97/pizzahub/src/usecase"
 )
 
 func main() {
 
-	chefHandler := handler.NewChefHTTPHandler()
-	menuHandler := handler.NewMenuHTTPHandler()
-	orderHandler := handler.NewOrderHTTPHandler()
+	chefRepository := repository.NewChefRepository()
+	chefUsecase := usecase.NewChefUsecase(chefRepository)
+	chefHandler := handler.NewChefHTTPHandler(chefUsecase)
+
+	menuRepository := repository.NewMenuRepository()
+	menuUsecase := usecase.NewMenuUsecase(menuRepository)
+	menuHandler := handler.NewMenuHTTPHandler(menuUsecase)
+
+	orderRepository := repository.NewOrderRepository()
+	orderUsecase := usecase.NewOrderUsecase(orderRepository)
+	orderHandler := handler.NewOrderHTTPHandler(orderUsecase)
 
 	http.HandleFunc("/chef", chefHandler.Chef)
 	http.HandleFunc("/menus", menuHandler.Menus)
